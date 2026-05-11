172cf09597275e55829530037a1f4beea81d740a
Никита Чванов <143615653+Persidzinho@users.noreply.github.com>
2026-05-11 15:12:21 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,30 @@
 // Copyright 2022 NNTU-CS
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <ctime>
 #include "train.h"
 
 int main() {
-  Train train;
-  int count = 60; // кол-во вагонов
-
-  while (count--)
-    train.addCar(false);
-
-  std::cout << train.getLength() << std::endl;
-  std::cout << train.getOpCount() << std::endl;
+  std::srand(std::time(nullptr));
+  std::ofstream out("result/data.csv");
+  out << "n,all_off,all_on,random\n";
+  for (int n = 2; n <= 200; n += 2) {
+    Train offTrain, onTrain, randTrain;
+    for (int i = 0; i < n; ++i) {
+      offTrain.addCar(false);
+      onTrain.addCar(true);
+      randTrain.addCar(std::rand() % 2 == 1);
+    }
+    offTrain.getLength();
+    int offOps = offTrain.getOpCount();
+    onTrain.getLength();
+    int onOps = onTrain.getOpCount();
+    randTrain.getLength();
+    int randOps = randTrain.getOpCount();
+    out << n << "," << offOps << "," << onOps << "," << randOps << "\n";
+  }
+  out.close();
+  std::cout << "Data saved to result/data.csv" << std::endl;
   return 0;
 }
