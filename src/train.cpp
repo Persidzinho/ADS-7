2267a0669803737715074805e6ddb866bf6e1479
Никита Чванов <143615653+Persidzinho@users.noreply.github.com>
2026-05-13 18:42:41 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -31,54 +31,25 @@ void Train::addCar(bool light) {
 int Train::getLength() {
   if (!first) return 0;
   countOp = 0;
-
-  bool allOff = true;
-  Car* tmp = first;
-  do {
-    if (tmp->light) {
-      allOff = false;
-      break;
-    }
-    tmp = tmp->next;
-  } while (tmp != first);
-
-  if (allOff) {
-    first->light = true;
+  first->light = true;
+  int k = 1;
+  while (true) {
     Car* cur = first;
-    int len = 0;
-    int laps = 0;
-    while (true) {
+    for (int i = 0; i < k; ++i) {
       cur = cur->next;
       ++countOp;
-      if (laps == 0) ++len;
-      if (cur == first) {
-        ++laps;
-        if (laps == 2) {
-          first->light = false;
-          return len;
-        }
-      }
-      if (cur->light) cur->light = false;
     }
-  } else {
-    first->light = true;
-    int k = 1;
-    while (true) {
-      Car* cur = first;
-      for (int i = 0; i < k; ++i) {
-        cur = cur->next;
-        ++countOp;
-      }
-      if (cur->light && cur != first) {
-        cur->light = false;
-      }
-      for (int i = 0; i < k; ++i) {
-        cur = cur->prev;
-        ++countOp;
-      }
-      if (!first->light) return k;
-      ++k;
+    if (cur->light) {
+      cur->light = false;
     }
+    for (int i = 0; i < k; ++i) {
+      cur = cur->prev;
+      ++countOp;
+    }
+    if (!first->light) {
+      return k;
+    }
+    ++k;
   }
 }
 
